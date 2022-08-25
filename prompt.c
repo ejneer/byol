707d98dc3cd933853ebe648993ed51866cd19c7c
Eric Neer <ericjneer@gmail.com>
2022-08-25 10:08:11 -0700

    use editline lib

--- prompt.c
+++ prompt.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+/*
+** had to add -ledit compilation flag
+** https://stackoverflow.com/a/23313923
+*/
+#include <editline/readline.h>
 
 int main(int argc, char **argv) {
 
@@ -11,14 +15,17 @@ int main(int argc, char **argv) {
 
   /* In a never ending loop */
   while (1) {
-    /* Output our prompt */
-    fputs("lispy> ", stdout);
+    /* Output our prompt and get input*/
+    char *input = readline("lispy> ");
 
-    /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
+    /* Add input to history */
+    add_history(input);
 
     /* Echo input back to the user */
-    printf("No you're a %s", input);
+    printf("No you're a %s\n", input);
+
+    /* Free retrieved input */
+    free(input);
   }
 
   return 0;
